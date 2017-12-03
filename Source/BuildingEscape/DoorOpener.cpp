9c1dbacfa07e260abd269e227cd104e2e071ae71
Dmitri Kolytchev <kolytchev.d@gmail.com>
2017-12-02 23:56:29 -0500

    Adding door and opener

--- Source/BuildingEscape/DoorOpener.cpp
+++ Source/BuildingEscape/DoorOpener.cpp
@@ -1,7 +1,7 @@
 // Dmitri Kolytchev 2017
 
 #include "DoorOpener.h"
-
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
 UDoorOpener::UDoorOpener()
@@ -18,17 +18,38 @@ UDoorOpener::UDoorOpener()
 void UDoorOpener::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	AActor* owner = GetOwner();
+	initialYaw = owner->GetActorRotation().Yaw;
+	OpenDoor();
 }
 
+void UDoorOpener::OpenDoor() {
+	isOpen = true;
+	start = GetWorld()->GetTimeSeconds();
+}
 
 // Called every frame
 void UDoorOpener::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// TBD: on interact
+	AActor* owner = GetOwner();
+	if (!isOpen) {
+		// open door if it's not open
+		UE_LOG(LogTemp, Warning, TEXT("%s is closed."), *owner->GetName());
+	} else {
+		FRotator rotator = owner->GetActorRotation();
+		float progression = fminf(1.f, (GetWorld()->GetTimeSeconds() - start) * closeSpeed);
+		UE_LOG(LogTemp, Warning, TEXT("%s is closing... %f%%"), *owner->GetName(), progression);
+
+		float newYaw = initialYaw + 90.f * (1.f - progression);
+		rotator.Yaw = newYaw;
+		owner->SetActorRotation(rotator);
+
+		if (progression >= 1.f) {
+			isOpen = false;
+		}
+	}
 }
 
--- Source/BuildingEscape/DoorOpener.h
+++ Source/BuildingEscape/DoorOpener.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Gameframework/Actor.h"
 #include "Components/ActorComponent.h"
 #include "DoorOpener.generated.h"
 
@@ -20,10 +21,15 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+public:
+	float closeSpeed = 1.0f;
+	void OpenDoor();
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	bool isOpen = false;
+	float start = 0.f;
+	float initialYaw = 0.f;
 	
 };
